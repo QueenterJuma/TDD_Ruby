73a24713e28ed44f5068f2e8eeb5a822e9a7f698
veronica365 <naruth10@gmail.com>
2023-05-25 17:23:48 +0300

    Remove un used Book befor each test

--- tests/factorial_spec.rb
+++ tests/factorial_spec.rb
@@ -1,7 +1,4 @@
 RSpec.describe 'factorial method of the solver class' do
-  before :each do
-    @book = Book.new 'Fashion Star', 'Queen'
-  end
   it 'should be able to calculate the factorial of 5' do
     expect(factorial(5)).to eq(120)
   end

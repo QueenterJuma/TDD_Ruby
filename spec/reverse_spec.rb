02e2dce30aa8420a263b476bec8bf785c2b9fcca
veronica365 <naruth10@gmail.com>
2023-05-25 18:20:44 +0300

    fix failing tests

--- spec/reverse_spec.rb
+++ spec/reverse_spec.rb
@@ -1,6 +1,7 @@
 RSpec.describe 'reverse a string  hello' do
-  before :each do
-    @reverse = Reverse.new 'olleh'
+  def reverse(string)
+    rev = Solver.new
+    rev.reverse(string)
   end
   it 'should be able to reverse a string  hello' do
     expect(reverse('hello')).to eq('olleh')
